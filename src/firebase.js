944e191659e0d4cef1add99d0b98fabb4117beb6
muuuuminn <hiro.041310402.kt@gmail.com>
2019-07-11 22:32:42 +0900

    feat: add firebase config

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyAQe0ctD8yABZxdev1apQIvQCN2thiSpMw',
+  authDomain: 'share-lyrics-view.firebaseapp.com',
+  databaseURL: 'https://share-lyrics-view.firebaseio.com',
+  projectId: 'share-lyrics-view',
+  storageBucket: 'share-lyrics-view.appspot.com',
+  messagingSenderId: '49423697571',
+});
+
+export default firebase;
